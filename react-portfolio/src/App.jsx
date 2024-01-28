589b7e5c975dba98deceb5a0d3a98ad1c985d0ad
sebswit <sebastian.switakowski@gmail.com>
2024-01-28 09:15:43 +0000

    added lines to App

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -6,17 +6,18 @@ import Header from './components/Header';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import ProjectGallery from './components/pages/ProjectGallery';
+import About from './components/pages/About';
 
 
 function App() {
   return (
     <Router>
-      <NavTabs />
-        <Header />
+        <NavTabs />
             <Routes>            
                 <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
                 <Route path="projectGallery" element={<ProjectGallery />} />                           
-                <Route path="contact/*" element={<Contact />} />
+                <Route path="contact" element={<Contact />} />
             </Routes>
         <Footer />
     </Router>
